426313afc4b41dde354ed055013d2caf246a694a
Tomasz Iwanicki <tiko35@o2.pl>
2023-06-19 21:02:20 +0200

    Initial commit

---
+++ src/main/java/org/seleniumfan/listidodawanieuzytkownikow/ListIDodawanieUzytkownikowApplication.java
@@ -0,0 +1,13 @@
+package org.seleniumfan.listidodawanieuzytkownikow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ListIDodawanieUzytkownikowApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ListIDodawanieUzytkownikowApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/seleniumfan/listidodawanieuzytkownikow/ListIDodawanieUzytkownikowApplicationTests.java
@@ -0,0 +1,13 @@
+package org.seleniumfan.listidodawanieuzytkownikow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ListIDodawanieUzytkownikowApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
